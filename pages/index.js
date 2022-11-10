339520efb16e42fe34050fe7599c3ef2d9c64c08
unknown <mirelasato10@gmail.com>
2022-11-09 22:19:50 -0300

    feat: add 'pink' mode

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import config from "../config.json";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
@@ -10,7 +9,6 @@ function HomePage() {
 
   return (
     <>
-      <CSSReset />
       <div style={{
         display: "flex",
         flexDirection: "column",
@@ -26,9 +24,10 @@ function HomePage() {
   );
 }
 
-export default HomePage
+export default HomePage;
 
 const StyledHeader = styled.div`
+background-color: ${({theme}) => theme.backgroundLevel1};
   img {
     width: 80px;
     height: 80px;
