07771acfff82aff0f80c74e814ad7c08287d97f8
unknown <mirelasato10@gmail.com>
2022-11-10 22:23:37 -0300

    fix: pipe

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import { CSSReset } from "../src/components/CSSReset";
 import React from 'react';
 import ColorModeProvider, { ColorModeContext } from "../src/components/Menu/components/ColorMode";
+import RegisterVideo from '../src/components/RegisterVideo';
 const theme = {
     light: {
         backgroundBase: "#f9f9f9",
@@ -43,6 +44,7 @@ function MyApp({ Component, pageProps }) {
             <ThemeProvider theme={theme[contexto.mode]} >
                 <CSSReset />
                 <Component {...pageProps} />
+                <RegisterVideo/>
             </ThemeProvider>
 
     );
---
+++ src/components/RegisterVideo/index.js
@@ -0,0 +1,14 @@
+import { StyledRegisterVideo } from './styles';
+
+export default function RegisterVideo() {
+    //falta btn para add
+    //Modal
+    // -> precisamos controlar o state
+    // -> formulario em si
+
+    return (
+        <StyledRegisterVideo>
+
+        </StyledRegisterVideo>
+    )
+}
