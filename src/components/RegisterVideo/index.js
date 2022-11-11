683d3c4fa9ee82d7e6ce36a6ac7e6b3008651708
unknown <mirelasato10@gmail.com>
2022-11-10 22:48:44 -0300

    fix: add form inicio

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,14 +1,31 @@
 import { StyledRegisterVideo } from './styles';
 
 export default function RegisterVideo() {
-    //falta btn para add
-    //Modal
+    // [x] falta btn para add
+    // [x] Modal
     // -> precisamos controlar o state
     // -> formulario em si
-
+    const formVisivel = false;
     return (
         <StyledRegisterVideo>
+            <button className="add-video"
+            >+</button>
+            {
+                formVisivel ?
+                    (
+                        <form>
+                            <div>
+                                <button className="close-modal">X</button>
+                                <input placeholder="Titulo do video" />
+                                <input placeholder="URL" />
+                                <button type="submit">Cadastrar</button>
+                            </div>
 
-        </StyledRegisterVideo>
+                        </form>
+                    )
+                    : null
+            }
+
+        </StyledRegisterVideo >
     );
 }
